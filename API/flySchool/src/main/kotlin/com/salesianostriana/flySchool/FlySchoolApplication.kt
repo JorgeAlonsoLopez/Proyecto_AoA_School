4198585b120473d01b241dcb27f6d0f3e20cfed8
Jorge Alonso <jorgealonso7@outlook.es>
2021-03-11 08:53:00 +0100

    Creación proyecto API

---
+++ API/flySchool/src/main/kotlin/com/salesianostriana/flySchool/FlySchoolApplication.kt
@@ -0,0 +1,11 @@
+package com.salesianostriana.flySchool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlySchoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlySchoolApplication>(*args)
+}
