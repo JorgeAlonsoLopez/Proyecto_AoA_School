1702ef5913f5f0584c84f62bbb87eb33b46af6d7
alonso.lojor20 <jorgealonso7@outlook.es>
2021-03-17 12:43:41 +0100

    tiempo

--- Android/app/src/main/java/com/salesianos/flyschool/ui/menu/MenuActivity.kt
+++ Android/app/src/main/java/com/salesianos/flyschool/ui/menu/MenuActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.TextureView
+import android.widget.TextView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -35,6 +37,10 @@ class MenuActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
+        var nombreUsuario : TextView = findViewById(R.id.text_menu_user)
+        nombreUsuario.text = "HOLAAAA"
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
 
